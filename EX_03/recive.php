f365f72a23257dc9c7e85a8793c3e11e140f6768
nil <ndelgadoa@campus.eug.es>
2022-10-06 10:44:54 +0200

    francisocccc

--- EX_03/recive.php
+++ EX_03/recive.php
@@ -4,8 +4,8 @@ if(!$_POST){
     header('Location: index.php');
 }
 $numero = $_POST['numero'];
-for ($i=0;$i<=$numero; $i++) {
-    echo 'la mama de la mama '. $numero.'</br>';
+for ($i=1;$i<=$numero; $i++) {
+    echo 'Hay '. $numero.'lineas'.'</br>';
 }
 
 
